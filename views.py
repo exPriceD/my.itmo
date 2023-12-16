43c1c0fdb79dcfbe4351ef63ab137a9da2b6624d
exPriceD <lolerpro4@gmail.com>
2023-12-16 15:21:33 +0300

    del chat update

--- views.py
+++ views.py
@@ -457,6 +457,9 @@ def delete_chat(current_user):
     chat = Chats.query.filter_by(id=id).first()
     if chat.first_member_id == current_user.id or chat.second_member_id == current_user.id:
         Chats.query.filter_by(id=id).delete()
+        messages = Messages.query.filter_by(chat_id=id).all()
+        for message in messages:
+            Messages.query.filter_by(id=message.id).delete()
         db.session.commit()
     response = {"status": 200}
     return Response(
