31d22b6bc108408cacd70b554991c3d3921b816c
exPriceD <lolerpro4@gmail.com>
2023-12-17 22:19:21 +0300

    bugs fixed

--- views.py
+++ views.py
@@ -473,8 +473,7 @@ def delete_chat(current_user):
         or chat.second_member_id == current_user.id
     ):
         Chats.query.filter_by(id=id).delete()
-        messages = Messages.query.filter_by(chat_id=id).all()
-        db.session.delete(messages)
+        db.session.query(Messages).filter(Messages.chat_id == id).delete()
         db.session.commit()
     response = {"status": 200}
     return Response(
